9ece2f0b10f095632fbf3e98e829ddad53a30d26
Md. Mashrur Kabir <113287768+Mashrur-Kabir@users.noreply.github.com>
2022-09-16 16:55:39 +0600

    Add files via upload

---
+++ P6. interchanging contents of two locations; c & d.c
@@ -0,0 +1,26 @@
+//the objective is to interchange the contents of two location called C & D//
+
+#include <stdio.h>
+
+int main () {
+
+    int c_location;
+    int d_location;
+    int blank_location;
+
+    printf ("enter the value of c= ");
+    scanf ("%d", &c_location);
+
+    printf ("enter the value of d= ");
+    scanf ("%d", &d_location);
+
+    blank_location=c_location;
+    c_location=d_location;
+    d_location=blank_location;
+
+    printf ("value of c location= %d", c_location);
+
+    printf ("\nvalue of d location= %d", d_location);
+
+    return 0;
+}
